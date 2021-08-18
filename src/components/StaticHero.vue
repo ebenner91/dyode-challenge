6bbd525bff89ef48e363acb276abe65da1fbf7b6
Elizabeth <elizabeth@btownweb.com>
2021-08-18 00:36:38 -0700

    Styling static hero

--- src/components/StaticHero.vue
+++ src/components/StaticHero.vue
@@ -2,9 +2,9 @@
     <div class="static-hero__relative-container">
         <div class="static-hero__hero-content"></div>
         <div class="static-hero__text-overlay">
-          <h2 class="static-hero__heading-text">Shop New Arrivals</h2>
+          <h2 class="static-hero__heading-text">Our Favorite Tees</h2>
           <p class="static-hero__info-text">
-            Our coolest new items are waiting for you!
+            Everyday tees you need
           </p>
           <a href="#" class="static-hero__cta-button">Shop Now</a>
         </div>
@@ -20,22 +20,60 @@
 .static-hero__hero-content {
   padding: 1px;
   height: 60vw;
-  background-image: url("../assets/hero-image-1.jpg");
+  background-image: url("../assets/static-hero-image.jpg");
   background-position: left;
   background-size: cover;
   background-repeat: no-repeat;
 }
 
+.static-hero__text-overlay {
+  background: #EFEFEF;
+  padding: 15px;
+  text-align: center;
+  color: #10504f;
+}
+
+.static-hero__heading-text {
+  font-family: 'Pacifico', cursive;
+  font-weight: 400;
+  font-size: 26px;
+  margin-bottom: 10px;
+}
+
+.static-hero__info-text {
+  font-size: 14px;
+  margin-top: 0;
+}
+
+.static-hero__cta-button {
+  text-decoration: none;
+  padding: 15px 20px;
+  background: #000;
+  color: #fff;
+  display: inline-block;
+}
+
 @media screen and (min-width: 1025px) {
   .static-hero__hero-content {
-    height: 80vh;
+    height: 100vh;
   }
 
   .static-hero__text-overlay {
     position: absolute;
-    top: 50%;
-    left: 130px;
-    transform: translateY(-50%);
+    top: 50px;
+    right: 50px;
+    background: none;
+    color: #fff;
+    text-align: left;
   }
+
+  .static-hero__heading-text {
+    font-size: 60px;
+  }
+
+  .static-hero__cta-button {
+    background: #10504f;
+  }
+
 }
 </style>
